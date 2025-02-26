225845380baa711e24744b47216fe81c90475e40
Nein243 <ysvalera@gmail.com>
2025-02-26 12:16:55 +0200

    проверка todo

--- team-view.php
+++ team-view.php
@@ -3,9 +3,10 @@ require_once 'header.php';
 require_once 'nav.php';
 require_once 'handler/function.php';
 $team = getOneById('teams', $_GET['id']);
-if (!isTeamOwner($_SESSION['id'], $team['owner_id'])){
+if (!isTeamOwner($_SESSION['id'], $team['owner_id'])) {
+// TODO СДЕЛАТЬ РЕДИРЕКТ НА СТРАНИЦУ 404
     redirect('deleted_list');
 }
 ?>
-<h1 class="main-header">Team <?=$team['title']?></h1>
+    <h1 class="main-header">Team <?= $team['title'] ?></h1>
 <?php require_once 'footer.php';
